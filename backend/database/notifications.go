f369f67c0a5594e9e31d95a8f6256beb82b89183
Ayoub-Benramdane <Ayoubbenramdane05@gmail.com>
2026-01-03 19:35:11 +0100

    Implement notifications system

--- backend/database/notifications.go
+++ backend/database/notifications.go
@@ -1,43 +1,162 @@
 package database
 
 import (
+	"fmt"
+	"sync"
+	"time"
+
 	structs "social-network/data"
 )
 
-func CreateNotification(user_id, notified_id, post_id, group_id, event_id int64, type_notification string) error {
-	_, err := DB.Exec("INSERT INTO notifications (user_id, notified_id, post_id, group_id, event_id, type_notification) VALUES (?, ?, ?, ?, ?, ?)", user_id, notified_id, post_id, group_id, event_id, type_notification)
+var (
+	ConnectedClients = structs.ConnectedClients
+	wsMutex          sync.Mutex
+)
+
+func sendWebSocketNotification(userID int64, payload map[string]interface{}) {
+	if clients, exists := ConnectedClients[userID]; exists {
+		for _, client := range clients {
+			if err := client.WriteJSON(payload); err != nil {
+				fmt.Println("websocket send error:", err)
+				return
+			}
+		}
+	}
+}
+
+func CreateNotification(actorUserID, receiverUserID, postID, groupID, eventID int64, notificationType string) error {
+	mu.Lock()
+	defer mu.Unlock()
+
+	_, err := Database.Exec(`
+		INSERT INTO notifications 
+		(user_id, notified_id, post_id, group_id, event_id, type_notification)
+		VALUES (?, ?, ?, ?, ?, ?)
+	`,
+		actorUserID, receiverUserID, postID, groupID, eventID, notificationType,
+	)
+
+	wsMutex.Lock()
+	sendWebSocketNotification(receiverUserID, map[string]interface{}{
+		"type": "notifications",
+	})
+	wsMutex.Unlock()
+
 	return err
 }
 
-func GetNotifications(notified_id, offset int64) ([]structs.Notification, error) {
+func GetNotifications(receiverUserID, offset int64) ([]structs.Notification, error) {
 	var notifications []structs.Notification
-	rows, err := DB.Query("SELECT n.id, u.username, u.avatar, n.post_id, n.group_id, n.event_id, n.type_notification, n.read, n.created_at FROM notifications n JOIN users u ON u.id = n.user_id WHERE n.notified_id = ? ORDER BY n.created_at DESC LIMIT ? OFFSET ?", notified_id, 10, offset)
+
+	rows, err := Database.Query(`
+		SELECT n.id, u.id, u.username, u.avatar,
+		       n.post_id, n.group_id, n.event_id,
+		       n.type_notification, n.read, n.created_at
+		FROM notifications n
+		JOIN users u ON u.id = n.user_id
+		WHERE n.notified_id = ?
+		ORDER BY n.created_at DESC
+		LIMIT 20 OFFSET ?
+	`, receiverUserID, offset)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
+
 	for rows.Next() {
 		var notification structs.Notification
-		err = rows.Scan(&notification.ID, &notification.User.Username, &notification.User.Avatar, &notification.PostID, &notification.GroupID, &notification.EventID, &notification.TypeNotification, &notification.Read, &notification.CreatedAt)
-		if err != nil {
+		var createdAt time.Time
+
+		if err := rows.Scan(&notification.NotificationID, &notification.UserID, &notification.Username, &notification.AvatarURL, &notification.PostID,
+			&notification.GroupID, &notification.EventID, &notification.NotificationType, &notification.IsRead, &createdAt); err != nil {
 			return nil, err
 		}
+
+		notification.CreatedAt = TimeAgo(createdAt)
+		notification.Message = buildNotificationMessage(notification.NotificationType)
 		notifications = append(notifications, notification)
 	}
+
 	return notifications, nil
 }
 
-func DeleteNotification(user_id, notified_id, post_id, group_id, event_id int64, type_notification string) error {
-	_, err := DB.Exec("DELETE FROM notifications WHERE user_id = ? AND notified_id = ? AND post_id = ? AND group_id = ? AND event_id = ? AND type_notification = ?", user_id, notified_id, post_id, group_id, event_id, type_notification)
+func buildNotificationMessage(notificationType string) string {
+	switch notificationType {
+	case "like":
+		return "liked your post"
+	case "comment":
+		return "commented on your post"
+	case "save":
+		return "saved your post"
+	case "follow":
+		return "started following you"
+	case "follow_request":
+		return "sent you a follow request"
+	case "group":
+		return "invited you to join a group"
+	case "join_request":
+		return "requested to join your group"
+	case "join":
+		return "joined your group"
+	case "event":
+		return "created an event"
+	default:
+		return "sent you a notification"
+	}
+}
+
+func CountUnreadNotifications(userID int64) (int64, error) {
+	var count int64
+	err := Database.QueryRow(`
+		SELECT COUNT(*) 
+		FROM notifications 
+		WHERE notified_id = ? AND read = 0
+	`, userID).Scan(&count)
+
+	return count, err
+}
+
+func DeleteNotification(actorUserID, receiverUserID, postID, groupID, eventID int64, notificationType string) error {
+	mu.Lock()
+	defer mu.Unlock()
+
+	_, err := Database.Exec(`
+		DELETE FROM notifications
+		WHERE user_id = ?
+		  AND notified_id = ?
+		  AND post_id = ?
+		  AND group_id = ?
+		  AND event_id = ?
+		  AND type_notification = ?
+	`,
+		actorUserID, receiverUserID, postID, groupID, eventID, notificationType,
+	)
+
 	return err
 }
 
-func MarkNotificationAsRead(user_id, notfication_id int64) error {
-	_, err := DB.Exec("UPDATE notifications SET read = 1 WHERE user_id = ? AND id = ?", user_id, notfication_id)
+func MarkNotificationAsRead(userID, notificationID int64) error {
+	mu.Lock()
+	defer mu.Unlock()
+
+	_, err := Database.Exec(`
+		UPDATE notifications
+		SET read = 1
+		WHERE notified_id = ? AND id = ?
+	`, userID, notificationID)
+
 	return err
 }
 
-func MarkAllNotificationsAsRead(user_id int64) error {
-	_, err := DB.Exec("UPDATE notifications SET read = 1 WHERE user_id = ?", user_id)
+func MarkAllNotificationsAsRead(userID int64) error {
+	mu.Lock()
+	defer mu.Unlock()
+
+	_, err := Database.Exec(`
+		UPDATE notifications
+		SET read = 1
+		WHERE notified_id = ?
+	`, userID)
+
 	return err
 }
--- backend/handlers/notifications.go
+++ backend/handlers/notifications.go
@@ -12,70 +12,116 @@ import (
 func NotificationsHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		fmt.Println("Method not allowed", r.Method)
-		response := map[string]string{"error": "Method not allowed"}
+		resp := map[string]string{"error": "Method not allowed"}
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		json.NewEncoder(w).Encode(response)
+		json.NewEncoder(w).Encode(resp)
 		return
 	}
 
-	user, err := GetUserFromSession(r)
-	if err != nil || user == nil {
+	currentUser, err := GetUserFromSession(r)
+	if err != nil || currentUser == nil {
 		fmt.Println("Failed to retrieve user", err)
-		response := map[string]string{"error": "Failed to retrieve user"}
+		resp := map[string]string{"error": "Failed to retrieve user"}
 		w.WriteHeader(http.StatusUnauthorized)
-		json.NewEncoder(w).Encode(response)
+		json.NewEncoder(w).Encode(resp)
 		return
 	}
 
-	offset, err := strconv.ParseInt(r.URL.Query().Get("offset"), 10, 64)
+	queryOffset, err := strconv.ParseInt(r.URL.Query().Get("offset"), 10, 64)
 	if err != nil {
 		fmt.Println("Error parsing offset:", err)
-		response := map[string]string{"error": "Invalid offset"}
+		resp := map[string]string{"error": "Invalid offset"}
 		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(response)
+		json.NewEncoder(w).Encode(resp)
 		return
 	}
 
-	notifications, err := database.GetNotifications(user.ID, offset)
+	notificationList, err := database.GetNotifications(currentUser.UserID, queryOffset)
 	if err != nil {
 		fmt.Println("Failed to retrieve notifications:", err)
-		response := map[string]string{"error": "Failed to retrieve notifications"}
+		resp := map[string]string{"error": "Failed to retrieve notifications"}
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(response)
+		json.NewEncoder(w).Encode(resp)
 		return
 	}
-	fmt.Println("Notifications:", notifications)
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(notifications)
+	json.NewEncoder(w).Encode(notificationList)
+}
+
+func MarkNotificationAsReadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		fmt.Println("Method not allowed", r.Method)
+		resp := map[string]string{"error": "Method not allowed"}
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	if !CheckLastActionTime(w, r, "notifications") {
+		return
+	}
+
+	currentUser, err := GetUserFromSession(r)
+	if err != nil || currentUser == nil {
+		fmt.Println("Failed to retrieve user", err)
+		resp := map[string]string{"error": "Failed to retrieve user"}
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	notificationID, err := strconv.ParseInt(r.URL.Query().Get("id"), 10, 64)
+	if err != nil {
+		fmt.Println("Error parsing notification ID:", err)
+		resp := map[string]string{"error": "Invalid notification ID"}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	if err := database.MarkNotificationAsRead(currentUser.UserID, notificationID); err != nil {
+		resp := map[string]string{"error": "Failed to mark notification as read"}
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	resp := map[string]string{"message": "Notification marked as read"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
 }
 
 func MarkNotificationsAsReadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		fmt.Println("Method not allowed", r.Method)
-		response := map[string]string{"error": "Method not allowed"}
+		resp := map[string]string{"error": "Method not allowed"}
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		json.NewEncoder(w).Encode(response)
+		json.NewEncoder(w).Encode(resp)
 		return
 	}
 
-	user, err := GetUserFromSession(r)
-	if err != nil || user == nil {
+	if !CheckLastActionTime(w, r, "notifications") {
+		return
+	}
+
+	currentUser, err := GetUserFromSession(r)
+	if err != nil || currentUser == nil {
 		fmt.Println("Failed to retrieve user", err)
-		response := map[string]string{"error": "Failed to retrieve user"}
+		resp := map[string]string{"error": "Failed to retrieve user"}
 		w.WriteHeader(http.StatusUnauthorized)
-		json.NewEncoder(w).Encode(response)
+		json.NewEncoder(w).Encode(resp)
 		return
 	}
 
-	if err := database.MarkAllNotificationsAsRead(user.ID); err != nil {
-		fmt.Println("Failed to mark notifications as read", err)
-		response := map[string]string{"error": "Failed to mark notifications as read"}
+	if err := database.MarkAllNotificationsAsRead(currentUser.UserID); err != nil {
+		resp := map[string]string{"error": "Failed to mark notifications as read"}
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(response)
+		json.NewEncoder(w).Encode(resp)
 		return
 	}
 
-	response := map[string]string{"message": "Notifications marked as read"}
+	resp := map[string]string{"message": "Notifications marked as read"}
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+	json.NewEncoder(w).Encode(resp)
 }
